c5e1e3e275e2c8f9398b55c8ff57726fc49569ba
Rohit Singh <rohitsingh18997@gmail.com>
2022-10-01 22:59:07 +0530

    initial commit

---
+++ src/main/java/com/example/custompetclinic/CustomPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.custompetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/custompetclinic/CustomPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custompetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
