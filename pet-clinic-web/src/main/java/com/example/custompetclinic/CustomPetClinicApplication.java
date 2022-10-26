0db9f6dbcac14113511d2dd0d193766a8a4fa5ac
Rohit Singh <rohitsingh18997@gmail.com>
2022-10-26 09:36:13 +0530

    adds index page and controllers

--- pet-clinic-web/src/main/java/com/example/custompetclinic/CustomPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/custompetclinic/CustomPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class CustomPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomPetClinicApplication.class, args);
     }
-
 }
